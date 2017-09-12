ac2f169d9a3b258cd46e3ba7163b03436f7bb641
DarushDev <darushfaqiri@gmail.com>
2017-09-12 22:46:57 +0800

    Add FirebaseInstanceIdService to generate unique IDs for each user or instance of the application

--- app/src/main/java/com/example/myfirebasenotifications/NotificationService.java
+++ app/src/main/java/com/example/myfirebasenotifications/NotificationService.java
@@ -10,6 +10,8 @@ import com.google.firebase.messaging.RemoteMessage;
 import java.util.Map;
 
 public class NotificationService extends FirebaseMessagingService {
+
+    String TAG = "mytag";
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
@@ -21,20 +23,20 @@ public class NotificationService extends FirebaseMessagingService {
         NotificationManagerCompat.from(this).notify(0, builder.build());
 
      for (Map.Entry<String, String> entry : remoteMessage.getData().entrySet()) {
-        Log.e("Test", "Key = " + entry.getKey() + ", Value = " + entry.getValue() );
+        Log.e(TAG, "Key = " + entry.getKey() + ", Value = " + entry.getValue() );
         }
 
-        Log.e("TutsPlus", "collapsekey: " + remoteMessage.getCollapseKey());
-        Log.e("TutsPlus", "from: " + remoteMessage.getFrom() );
-        Log.e("TutsPlus", "message id: " + remoteMessage.getMessageId() );
-        Log.e("TutsPlus", "message type:: " + remoteMessage.getMessageType() );
-        Log.e("TutsPlus", "to: " + remoteMessage.getTo() );
-        Log.e("TutsPlus", "send time: " + remoteMessage.getSentTime() );
-        Log.e("TutsPlus", "ttl: " + remoteMessage.getTtl() );
-        Log.e("TutsPlus", "title: " + remoteMessage.getNotification().getTitle() );
-        Log.e("TutsPlus", "body: " + remoteMessage.getNotification().getBody() );
-        Log.e("TutsPlus", "click action: " + remoteMessage.getNotification().getClickAction() );
-        Log.e("TutsPlus", "color: " + remoteMessage.getNotification().getColor() );
-        Log.e("TutsPlus", "icon: " + remoteMessage.getNotification().getIcon() );
+        Log.e(TAG, "collapsekey: " + remoteMessage.getCollapseKey());
+        Log.e(TAG, "from: " + remoteMessage.getFrom() );
+        Log.e(TAG, "message id: " + remoteMessage.getMessageId() );
+        Log.e(TAG, "message type:: " + remoteMessage.getMessageType() );
+        Log.e(TAG, "to: " + remoteMessage.getTo() );
+        Log.e(TAG, "send time: " + remoteMessage.getSentTime() );
+        Log.e(TAG, "ttl: " + remoteMessage.getTtl() );
+        Log.e(TAG, "title: " + remoteMessage.getNotification().getTitle() );
+        Log.e(TAG, "body: " + remoteMessage.getNotification().getBody() );
+        Log.e(TAG, "click action: " + remoteMessage.getNotification().getClickAction() );
+        Log.e(TAG, "color: " + remoteMessage.getNotification().getColor() );
+        Log.e(TAG, "icon: " + remoteMessage.getNotification().getIcon() );
     }
 }
